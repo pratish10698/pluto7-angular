676d61f16916989fe5aa6ab1d3c16087f8adb426
Pratish Kumar <pratishkumar@Pratishs-Air.hgu_lan>
2022-03-29 14:39:15 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { TodosComponent } from './my_components/todos/todos.component';
+import { TodoItemComponent } from './my_components/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    TodosComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/my_components/todos/todos.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent implements OnInit {
+  todos:Todo[];
+  constructor() { 
+    this.todos = [
+      {
+        sno:1,
+        title:"thisi is title",
+        desc: "Description",
+        active:true
+      },
+      {
+        sno:2,
+        title:"thisi is title 2",
+        desc: "Description 2",
+        active:true
+      },
+      {
+        sno:3,
+        title:"thisi is title 3 ",
+        desc: "Description 3",
+        active:true
+      }
+    ]
+  }
+
+  ngOnInit(): void {
+  }
+
+}
